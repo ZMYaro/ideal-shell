5840aee9452f15f544a2e80c8cfaab7d807bd637
Zachary Yaro <zmyaro@zmyaro.com>
2020-08-30 04:36:25 -0400

    Add app icons and metadata

---
+++ scripts/app-list.js
@@ -0,0 +1,47 @@
+export const APP_ICON_DIR = 'images/app_icons/';
+export const APPS = [
+	{
+		name: 'Gmail',
+		shortName: 'Gmail',
+		url: 'https://mail.google.com/mail/mu',
+		id: 'gmail',
+		tileTextColor: 'dark'
+	}, {
+		name: 'Google Keep',
+		shortName: 'Keep',
+		url: 'https://keep.google.com',
+		id: 'gkeep',
+		tileTextColor: 'light'
+	}, {
+		name: 'Google Maps',
+		shortName: 'Maps',
+		url: 'https://maps.google.com',
+		id: 'gmaps',
+		tileTextColor: 'light'
+	}, {
+		name: 'Instagram',
+		url: 'https://www.instagram.com',
+		id: 'instagram',
+		tileTextColor: 'mixed'
+	}, {
+		name: 'PaintZ',
+		url: 'https://paintz.app',
+		id: 'paintz',
+		tileTextColor: 'light'
+	}, {
+		name: 'Slack',
+		url: 'https://app.slack.com/client',
+		id: 'slack',
+		tileTextColor: 'dark'
+	}, {
+		name: 'Twitter',
+		url: 'https://twitter.com',
+		id: 'twitter',
+		tileTextColor: 'light'
+	}, {
+		name: 'YouTube',
+		url: 'https://www.youtube.com',
+		id: 'youtube',
+		tileTextColor: 'dark'
+	}
+];
